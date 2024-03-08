2817850aee15e692d7bccac71e3813f53eb940ce
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-03-08 17:47:10 -0500

    database and config module are loaded properly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from '@app/common/database/database.module';
+import { ConfigModule } from '@app/common/config/config.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
