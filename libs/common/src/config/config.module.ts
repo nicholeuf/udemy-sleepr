c1696cb1899a2feb4d76c656b29221ea8d3787d0
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-03-08 20:38:25 -0500

    Common Library and Reservations (#1)
    * database and config module are loaded properly
    
    * database & config modules, abstract repository, reservations crud
    
    * validation & logging
    
    * dockerize

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,19 @@
+import * as Joi from 'joi';
+import { Module } from '@nestjs/common';
+import {
+  ConfigModule as NestConfigModule,
+  ConfigService,
+} from '@nestjs/config';
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigModule {}
