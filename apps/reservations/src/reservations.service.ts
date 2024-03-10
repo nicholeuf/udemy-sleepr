a07f722b5ab19ceb819accc898e5b725e44c0275
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-03-10 16:48:43 -0400

    save progress on completing the stripe charge (there is a stripe error about sending raw credit card details)

--- apps/reservations/src/reservations.service.ts
+++ apps/reservations/src/reservations.service.ts
@@ -9,15 +9,20 @@ import { ClientProxy } from '@nestjs/microservices';
 export class ReservationsService {
   constructor(
     private readonly reservationsRepository: ReservationsRepository,
-    @Inject(PAYMENTS_SERVICE) paymentsService: ClientProxy,
+    @Inject(PAYMENTS_SERVICE) private readonly paymentsService: ClientProxy,
   ) {}
 
   async create(createReservationDto: CreateReservationDto, userId: string) {
-    return this.reservationsRepository.create({
-      ...createReservationDto,
-      timestamp: new Date(),
-      userId,
-    });
+    this.paymentsService
+      .send('create_charge', createReservationDto.charge)
+      .subscribe(async (response) => {
+        console.log(response);
+        return await this.reservationsRepository.create({
+          ...createReservationDto,
+          timestamp: new Date(),
+          userId,
+        });
+      });
   }
 
   async findAll() {
