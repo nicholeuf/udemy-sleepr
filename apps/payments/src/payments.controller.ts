03e9272fd9f66b515f02f8b31960ee53fdbc934d
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-03-10 13:33:56 -0400

    stripe setup

---
+++ apps/payments/src/payments.controller.ts
@@ -0,0 +1,14 @@
+import { Controller } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateChargeDto } from './dto/create_charge.dto';
+
+@Controller()
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @MessagePattern('create_charge')
+  async createCharge(@Payload() data: CreateChargeDto) {
+    this.paymentsService.createCharge(data);
+  }
+}
