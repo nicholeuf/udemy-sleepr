6c47d4329c8f32e0996be96edf88891841b1c8d6
Nichole Frey <nicholeuf@users.noreply.github.com>
2024-03-26 14:51:02 -0400

    Section 12: Rabbitmq (#16)
    * remove app pnpm lock files
    
    * add rabbitmq deps
    
    * add rabbitmq image to docker compose
    
    * services are wired to rabbitmq rather than tcp
    
    * payments ack the message once the charge is completed

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,6 +1,11 @@
 import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 
 @Controller()
@@ -9,7 +14,16 @@ export class PaymentsController {
 
   @MessagePattern('create_charge')
   @UsePipes(new ValidationPipe())
-  async createCharge(@Payload() data: PaymentsCreateChargeDto) {
-    return this.paymentsService.createCharge(data);
+  async createCharge(
+    @Payload() data: PaymentsCreateChargeDto,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    const charge = this.paymentsService.createCharge(data);
+    channel.ack(originalMsg);
+
+    return charge;
   }
 }
